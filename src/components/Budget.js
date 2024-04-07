b6e74a2afa1b9b58b28f729aafe533c8efc34b19
AEPALO00 <palomaresv.ae@gmail.com>
2024-04-07 18:08:09 -0400

    validation and create dropdown js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,36 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    // Import app context
+    const { budget, expenses } = useContext(AppContext);
+    //  Create hooks
+    const [newBudget, setNewBudget] = useState(budget);
+    const handleBudgetChange = (event) => {
+        const totalExpenses = expenses.reduce((total, item) => {
+            return (total = total + item.cost);
+        }, 0);
+        console.log(totalExpenses)
+
+        if (event.target.value <= 20000){
+            if (event.target.value <= totalExpenses) {
+                alert(`Budget must not be lower than expenses!`);
+                document.getElementById("in_budget").value = totalExpenses;
+            } else {
+                setNewBudget(event.target.value);
+            }
+        } else {
+            alert("Budget must not exceed 20,000!");
+            document.getElementById("in_budget").value = newBudget;
+        }
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £ </span>
+            <input value={newBudget} id="in_budget" type='number' step="10" onChange={handleBudgetChange}></input>
+        </div>
+    );
+}
+
+export default Budget;
